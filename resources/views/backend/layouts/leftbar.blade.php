80525bbfcd6c049a7b4746eeabba896515610519
Shad <shadjannat1@gmail.com>
2026-01-28 10:21:13 -0800

    first commit

--- resources/views/backend/layouts/leftbar.blade.php
+++ resources/views/backend/layouts/leftbar.blade.php
@@ -11,7 +11,7 @@
       
       <!-- sidebar menu -->
       <ul class="sidebar-menu" data-widget="tree">
-        <li class="treeview"> <a href="#"> <i class="ti-email"></i> <span>Products</span> <span class="pull-right-container"> <i class="fa fa-angle-left pull-right"></i> </span> </a>
+        <li class="treeview"> <a href="#"> <i class="ti-email"></i> <span>Dashboard</span> <span class="pull-right-container"> <i class="fa fa-angle-left pull-right"></i> </span> </a>
           <ul class="treeview-menu">
             <li><a href="/product"><i class="fa fa-angle-right"></i> All Products</a></li>
             <li><a href="/product/create"><i class="fa fa-angle-right"></i> Add Product</a></li>
